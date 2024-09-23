a17e18222e471d0f393c3d9af1d39d1907c433dd
potato <130343257+terminaterpanda@users.noreply.github.com>
2024-09-23 18:11:27 +0900

    addding+

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,12 @@
-#1. algorithm 틀 짜기
+# 1. BFS
+
+# 1-1. BFS [with no collections package]
 class Node:
     def __init__(self, data):
         if not data:
             raise ValueError("nothing is imported")
         self.data = data
-        self.next = None
-        self.rear = None
+        self.neighbors = [] #다중연결 == "list"
         
 class MakeNode:
     def __init__(self, *args):
@@ -14,13 +15,70 @@ class MakeNode:
         self.lists = {}
         
         for i, value in enumerate(args, start=1):
-            current = Node(value)
-            self.lists[i] = current
+            current = Node(value) #전달 value값을 이용해서 node 객체 생성
+            self.lists[i] = current #node를 dictionary self.lists에 저장
 
-        print(current)
+    def connect(self, key1, key2):
+        node1 = self.lists[key1]
+        node2 = self.lists[key2]
 
-MakeNode(3,4,5,6,7)
+        node1.neighbors.append(node2)
+        node2.neighbors.append(node1) #양방향 연결
 
-     
-
+    def bfs(self, start_key):
+        if start_key not in self.lists:
+            raise ValueError("error")
         
+        start_node = self.lists[start_key]
+        visited = set() #visted를 set(집합) 형식으로 만들어서 value 값 부여.
+        queue = [start_node]
+
+        while queue: #queue가 비어있지 않은 한 끊임없이 repeat.
+            current_node = queue.pop(0)
+            
+            if current_node not in visited:
+                print(f"node: {current_node.data}")
+                visited.add(current_node)
+                #node를 visited <set(집합)> 에 추가하여 중복 방지.
+
+                for neighbor in current_node.neighbors:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+        if not queue:
+            print("visited everythng")
+            queue.clear()
+           
+
+# 1-2. BFS [collections package]
+
+from collections import deque
+
+def bfs (graph, node, visited):
+    queue = deque([node])
+    visited[node] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        #처리 중 node에서 방문하지 않은 node(인접) 전부 queue에 삽입
+        for i in graph[v]:
+            if not (visited[i]):
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    [],       
+    [2, 3],   
+    [1, 8],   
+    [1, 5],   
+    [],       
+    [3],      
+    [],      
+    [],       
+    [2]       
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
