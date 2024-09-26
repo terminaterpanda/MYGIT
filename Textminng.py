524e6ec17fe83dd5abd78fdf0586c913653310c5
potato <130343257+terminaterpanda@users.noreply.github.com>
2024-09-26 17:19:24 +0900

    12

--- Textminng.py
+++ Textminng.py
@@ -4,18 +4,15 @@ from konlpy.tag import Okt
 from sklearn.feature_extraction.text import CountVectorizer
 import re
 
-file_path1 = "/Users/iseong-yong/Desktop/files/movie1.csv"
- 
+file_path1 = "/Users/iseong-yong/Desktop/files/movie1.csv" 
 textfile = pd.read_csv(file_path1, encoding= "utf-8")
 """
 with open(file_path1, "r) as file:
     textfile = file.read() <when not using pandas library>
 """
-textfile_1 = textfile[textfile.point < 5]
-#580*3 matrix
-textfile_2 = textfile[~textfile.index.isin(textfile_1.index)]
-#이런 식으로 위의 조건을 제외.
-#420*3 matrix
+textfile_1 = textfile[textfile.point < 5] #580*3 matrix
+textfile_2 = textfile[~textfile.index.isin(textfile_1.index)] #420*3 matrix
+
 t1 = textfile_1.iloc[0:580,2].reset_index(drop=True)
 t2 = textfile_2.iloc[0:420,2].reset_index(drop=True)
 #rest_index == 인덱스를 재정렬해주는 function.
@@ -26,11 +23,15 @@ texter2 = " ".join(t2.astype(str))
 
 nouns = okt.nouns(texter)
 nouns1 = okt.nouns(texter2)
+
+#명사 빈도수 계산
 nouns_freq = pd.Series(nouns).value_counts()
 nouns_freq1 = pd.Series(nouns1).value_counts()
 #value_counts == 기본적으로 지정된 열의 값들에 대한 모든 밞생 횟수를 반환.
+
 nouns_df = nouns_freq.reset_index()
 nouns_df2 = nouns_freq1.reset_index()
+
 nouns_df.columns = ["noun", "frequency"]
 nouns_df2.columns = ["noun", "frequency"]
 
@@ -42,12 +43,19 @@ output_file_path1 = "/Users/iseong-yong/Desktop/files/nouns_frequency_positive.c
 
 nouns_df.to_csv(output_file_path, index=False, encoding='utf-8-sig')
 nouns_df2.to_csv(output_file_path1, index=False, encoding="utf-8-sig")
-#nouns_df == 평점 5점 이하 
-#nouns_df2 == 평점 5 초과 (vector 임베딩)
+#nouns_df == 평점 5점 미만
+#nouns_df2 == 평점 5 이상 (vector 임베딩)
 
 stopwords = ['이', '있', '하', '것', '들', '그', '되', '수', '이', '보', '않', '없', '나', '사람', '주', '아니', '등', '같', '우리', '때', '년', '가', '한', '지', '대하', '오', '말', '일', '그렇', '위하']
 
-print(len(stopwords))
+filteredn = nouns_df[~nouns_df["noun"].isin(stopwords)]
+filteredp = nouns_df2[~nouns_df2["noun"].isin(stopwords)]
+#match == "re에서 사용" 다른 곳에서는 isin use.
 
-print(type(nouns_df))
-print(type(nouns_df2))
+file_path3 = "/Users/iseong-yong/Desktop/files/negative.csv"
+file_path4 = "/Users/iseong-yong/Desktop/files/positive.csv"
+
+filteredn.to_csv(file_path3, index=False, encoding="utf-8-sig")
+filteredp.to_csv(file_path4, index=False, encoding="utf-8-sig")
+
+
