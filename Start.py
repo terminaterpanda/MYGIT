31def25116f75b59a05e11b04ebfef589723ebc8
potato <130343257+terminaterpanda@users.noreply.github.com>
2024-12-05 13:45:56 +0900

    1223

--- Start.py
+++ Start.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import nltk
 nltk.download("punkt")
+nltk.download("stopwords")
 
 #numpy = (1.ndim == 축의 개수) / (2.shape == 크기)
 
@@ -273,7 +274,7 @@ from konlpy.tag import Okt
 
 #1. 정수 인코딩 수행
 #2. 1과 0을 부여하여 one-hot vectorize.
-okt = Okt
+okt = Okt()
 tokens = okt.morphs("나는 밥을 먹는다")
 print(tokens)
 
@@ -335,4 +336,82 @@ print(len(av_train))
 print(len(av_test))
 
 #keras importing + modeling.
+from keras_preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+
+tokenizer = Tokenizer()
+train_text = "The earth is an awesome place live"
+
+tokenizer.fit_on_texts([train_text])
+sub_text = "The earth is an awesome place live"
+sequences = tokenizer.texts_to_sequences([sub_text])[0]
+
+print(sequences)
+print(tokenizer.word_index)
+#pad_sequences -> zero-padding(0을 넣어서 길이 계산 맞춤)
+
+pad_sequences([1,2,3], [3,4,5,6], [7,8], maxlen=3,padding="pre")
+#maxlen -> data에 대해서 정규화할 길이
+#padding = "pre"(앞에 0을 채움) / "padding = "post"(뒤에 0을 채움)
+
+#Embedding() -> 단어를 밀집 vector(원핫 인코딩 임베딩은 너무 크므로 밀집 vector로 표현)
+
+tokenized_text = [['Hope', 'to', 'see', 'you', 'soon'], ['Nice', 'to', 'see', 'you', 'again']]
+
+encoded_text = [[0, 1, 2, 3, 4],[5,1,2,3,6]]
+
+vocab_size = 7
+embedding_dim = 2
+
+import tensorflow as tf
+
+class LinearRegression(tf.keras.Model):
+    def __init__(self):
+        super(LinearRegression, self).__init__()
+        self.linear_layer = tf.keras.layers.Dense(1, input_dim =1,
+                                                  activation = "linear")
+    def call(self, x):
+        y_pred = self.linear_layer(x)
+
+        return y_pred
+    
+model = LinearRegression()
+
+#MLP(다층 퍼셉트론)
+import numpy as np
+from keras_preprocessing.text import Tokenizer
+
+texts = ['먹고 싶은 사과', '먹고 싶은 바나나', '길고 노란 바나나 바나나', '저는 과일이 좋아요']
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(texts)
+print(tokenizer.word_index)
+
+#texts_to_matrix
+print(tokenizer.texts_to_matrix(texts, mode="count"))
+#texts를 input으로 넣고 모드는 "count"를 use.
+
+print(tokenizer.texts_to_matrix(texts, mode="binary"))
+#해당 단어 존재 여부
+
+print(tokenizer.texts_to_matrix(texts, mode = "tfidf").round(2))
+#tf-idf
+
+print(tokenizer.texts_to_matrix(texts, mode = "freq").round(2))
+#각 단어 등장 횟수 -> 분자
+#각 문서 크기 -> 분모  
+# -> 이런 식의 분석 way.
+
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+import matplotlib.pyplot as plt
+from keras_preprocessing.text import Tokenizer
+from keras_utils import to_categorical
+
+newsdata = fetch_20newsgroups(subset="train")
+
+print(newsdata.keys())
+print("훈련용 샘플의 개수 : {}".format(len(newsdata.data)))
+
+print("총 주제의 개수 : {}".format(len(newsdata.target_names)))
+print(newsdata.target_names)
 
